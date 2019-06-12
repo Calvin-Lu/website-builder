6b31c88872b313a3f6f7bc3323aa3a3a31fcf67e
Jackson Zheng <jacksonzheng@Jacksons-MacBook-Pro.local>
2019-06-12 17:02:37 -0400

    Created function to drag elements

---
+++ frontend-website-builder/index.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    // Make the DIV element draggable:
+    dragElement(document.querySelector("#resize-test2"));
+}
+
+// function dragElement(elmnt) {
+//   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+//   elmnt.onmousedown = dragMouseDown;
+// //   if (document.getElementById(elmnt.id + "header")) {
+// //     // if present, the header is where you move the DIV from:
+// //     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+// //   } else {
+// //     // otherwise, move the DIV from anywhere inside the DIV: 
+   
+// //   }
+
+// }
+
+class Element {
+    constructor(elmnt) {
+        this.dragElement(elmnt);
+    }
+
+    dragElement(elmnt) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        elmnt.addEventListener("mousedown", dragMouseDown)
+    
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            this.addEventListener("mouseup", closeDragElement);
+            this.addEventListener("mousemove", elementDrag);
+          }
+        
+          function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            
+            // console.log(elmnt.offsetTop - pos2);
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+          }
+        
+          function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        
+            this.removeEventListener("mouseup", closeDragElement);
+            this.removeEventListener("mousemove", elementDrag);
+          }
+    }
+}
+
+
+
+function resizeable(e) {
+    // alert("hello")
+    this.style.resize = "both";
+    console.log(this)
+}
+
