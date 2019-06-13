971c0572e739190102f038075a23dbd49fa7375b
Jackson Zheng <jacksonzheng@Jacksons-MacBook-Pro.local>
2019-06-12 20:52:29 -0400

    updated drag feature for element class

--- frontend-website-builder/index.js
+++ frontend-website-builder/index.js
@@ -6,55 +6,50 @@ function main() {
 }
 
 class Element {
-    constructor(elmnt) {
-        this.dragElement(elmnt);
-    }
+  constructor(element) {
+      this.element = element;
+      this.element.addEventListener("mousedown", this.dragMouseDown.bind(this));
+      this.element.style.position = "absolute";
+      this.positions = {pos1: 0, pos2: 0, pos3: 0, pos4: 0};
+  }
 
-    dragElement(elmnt) {
-        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        elmnt.addEventListener("mousedown", dragMouseDown)
+  dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    this.positions.pos3 = e.clientX;
+    this.positions.pos4 = e.clientY;
+    this.element.addEventListener("mouseup", this.closeDragElement.bind(this));
+    this.element.addEventListener("mousemove", this.elementDrag.bind(this));
+  }
+  
+  elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    this.positions.pos1 = this.positions.pos3 - e.clientX;
+    this.positions.pos2 = this.positions.pos4 - e.clientY;
+    this.positions.pos3 = e.clientX;
+    this.positions.pos4 = e.clientY;
     
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            this.addEventListener("mouseup", closeDragElement);
-            this.addEventListener("mousemove", elementDrag);
-          }
-        
-          function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            
-            // console.log(elmnt.offsetTop - pos2);
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-          }
-        
-          function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-        
-            this.removeEventListener("mouseup", closeDragElement);
-            this.removeEventListener("mousemove", elementDrag);
-          }
-    }
-}
+    // console.log(element.offsetTop - pos2);
+    // set the element's new position:
+    this.element.style.top = (this.element.offsetTop - this.positions.pos2) + "px";
+    this.element.style.left = (this.element.offsetLeft - this.positions.pos1) + "px";
+  }
+  
+  closeDragElement() {
+    // stop moving when mouse button is released:
+    this.element.removeEventListener("mouseup", this.closeDragElement.bind(this));
+    this.element.removeEventListener("mousemove", this.elementDrag.bind(this));
+  }
 
-
-
-function resizeable(e) {
+  resizeable(e) {
     // alert("hello")
     this.style.resize = "both";
-    console.log(this)
+    this.removeEventListener("mousedown", this.dragMouseDown);
+  }
 }
+
+
 
